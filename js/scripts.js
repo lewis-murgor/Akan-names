433914cd4b5278637b2b3e6279494a08c63fd578
Lewis <lewis.murgor@student.moringaschool.com>
2022-01-10 12:56:06 +0300

    modify

--- js/scripts.js
+++ js/scripts.js
@@ -4,10 +4,9 @@ function dayOfWeek(date, month, century, year) {
     var year = document.getElementById("year").value;
     var century = document.getElementById("century").value;
 
-    return ( ( (century/4) -2*century-1) + ((5*year/4) ) + ((26*(month+1)/10)) + date ) % 7
+    return ( ( (century/4) -2*century-1) + ((5*year/4) ) + ((26*(month+1)/10)) + date ) % 7;
+}
+function getAkanName() {
+    var days = ["Sunday","Monday","Tuesday",]
 }
 
-function getAkanName() {
-    var days = ["sunday","monday","tuesday","wednesday","thursday","friday","saturday"];
-    
-}
