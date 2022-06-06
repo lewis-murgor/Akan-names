b00f47a8d245d222e3f5e35251a1b5fe4a2bf079
Lewis <lewis.murgor@student.moringaschool.com>
2022-06-06 17:45:08 +0300

    add changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,40 +1,111 @@
-function getAkanName(cc, yy, mm, dd) {
-    return(((cc/4) -2*cc-1) + ((5*yy/4)) + ((26*(mm+1)/10)) + dd )% 7
-    var cc = document.getElementById("century").val();
-    var yy = document.getElementById("year").val();
-    var mm = document.getElementById("month").val();
-    var dd = document.getElementById("date").val();
+function getAkanName () {
 
-    var gender = document.getElementById("sex").val();
+    var validateDate = document.getElementById('date').value;
 
-    if ((getAkanName===0) && (gender===male)) {
-        alert("You were born on Sunday therefore your akan name is Kwasi");
-    } else if ((getAkanName===0) && (gender===female)) {
-        alert("You were born on Sunday therefore your akan name is Akosua");
-    } else if ((getAkanName===1) && (gender===male)) {
-        alert("You were born on Monday therefore your akan name is Kwadwo");
-    } else if ((getAkanName===1) && (gender===female)) {
-        alert("You were born on Monday therefore your akan name is Adwoa");
-    } else if ((getAkanName===2) && (gender===male)) {
-        alert("You were born on Tuesday therefore your akan name is Kwabena");
-    } else if ((getAkanName===2) && (gender===female)) {
-        alert("You were born on Tuesday therefore your akan name is Abenaa");
-    } else if ((getAkanName===3) && (gender===male)) {
-        alert("You were born on Wednesday therefore your akan name is Kwaku");
-    } else if ((getAkanName===3) && (gender===female)) {
-        alert("You were born on Wednesday therefore your akan name is Akua");
-    } else if ((getAkanName===4) && (gender===male)) {
-        alert("You were born on Thursday therefore your akan name is Yaw");
-    } else if ((getAkanName===4) && (gender===female)) {
-        alert("You were born on Thursday therefore your akan name is Yaa");
-    } else if ((getAkanName===5) && (gender===male)) {
-        alert("You were born on Friday therefore your akan name is Kofi");
-    } else if ((getAkanName===5) && (gender===female)) {
-        alert("You were born on Friday therefore your akan name is Afua");
-    } else if ((getAkanName===6) && (gender===male)) {
-        alert("You were born on Saturday therefore your akan name is Kwame");
+    if (validateDate < 1 || validateDate > 31) {
+        alert("Input a valid date");
     } else {
-        alert("You were born on Saturday therefore your akan name is Ama");
+        validateDate;
+    }
+
+    var validateMonth = document.getElementById('month').value;
+
+    if (validateMonth < 1 || validateMonth > 12) {
+        alert('Input a valid month');
+    } else {
+        validateMonth;
+    }
+
+    var year = document.getElementById('year').value;
+    var cc = year.slice(0, 2);
+    var yy = year.slice(2, 4);
+
+    var day = Math.floor( ( (cc/4) -2*cc-1) + ((5*yy/4) ) + ((26*(validateMonth+1)/10)) + validateDate ) % 7;
+
+    let male;
+    let female;
+
+    if (document.getElementById("male").checked) {
+        male = true;
+    } else if (document.getElementById("female").checked) {
+        female = true;
+    } else {
+        alert("You didn't input any gender value");
+    }
+
+    var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+    var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+
+    if (male && validateDate && validateMonth) {
+        switch(day) {
+            case 1:
+                alert(`You were born on a Sunday therefore your Akan name is ${maleNames[0]}`);
+                return false;
+                break;
+            case 2:
+                alert(`You were born on a Monday therefore your Akan name is ${maleNames[1]}`);
+                return false;
+                break;
+            case 3:
+                alert(`You were born on a Tuesday therefore your Akan name is ${maleNames[2]}`);
+                return false;
+                break;
+            case 4:
+                alert(`You were born on a Wednesday therefore your Akan name is ${maleNames[3]}`);
+                return false;
+                break;
+            case 5:
+                alert(`You were born on a Thursday therefore your Akan name is ${maleNames[4]}`);
+                return false;
+                break;
+            case 6:
+                alert(`You were born on a Friday therefore your Akan name is ${maleNames[5]}`);
+                return false;
+                break;
+            case 7:
+                alert(`You were born on a Saturday therefore your Akan name is ${maleNames[6]}`);
+                return false;
+                break;
+            
+            default:
+                alert("Sorry we can't find your name");
+        }
+    } else if (female && validateDate && validateMonth) {
+        switch(day) {
+            case 1:
+                alert(`You were born on a Sunday therefore your Akan name is ${femaleNames[0]}`);
+                return false;
+                break;
+            case 2:
+                alert(`You were born on a Monday therefore your Akan name is ${femaleNames[1]}`);
+                return false;
+                break;
+            case 3:
+                alert(`You were born on a Tuesday therefore your Akan name is ${femaleNames[2]}`);
+                return false;
+                break;
+            case 4:
+                alert(`You were born on a Wednesday therefore your Akan name is ${femaleNames[3]}`);
+                return false;
+                break;
+            case 5:
+                alert(`You were born on a Thursday therefore your Akan name is ${femaleNames[4]}`);
+                return false;
+                break;
+            case 6:
+                alert(`You were born on a Friday therefore your Akan name is ${femaleNames[5]}`);
+                return false;
+                break;
+            case 7:
+                alert(`You were born on a Saturday therefore your Akan name is ${femaleNames[6]}`);
+                return false;
+                break;
+            
+            default:
+                alert("Sorry we can't find your name");
+        }
+    } else {
+        alert("You entered invalid month or day");
     }
 }
 
